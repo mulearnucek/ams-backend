f590b44ea4b433026996cf59c00a3a78e5383374
chethas.lp <lchethas@gmail.com>
2025-11-27 15:20:29 +0530

    UPD: health endpoints

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,19 +1,30 @@
 'use strict'
 
 import { FastifyInstance } from "fastify"
+import os from "os"
 
 export default async function (fastify: FastifyInstance) {
-  fastify.get('/', async function (request, reply) {
-    return { message: "Hi User! What brings you here? (◔◡◔) - GQA API Server", v: '1.0.0', status: 200 }
-  })
-
-  fastify.get('/health', async function (request, reply) {
-    return { message: "I'm alive!" , status: 200}
-  })
 
   fastify.setNotFoundHandler(function (req, reply) {
     reply.code(404).send({ message: 'You are looking at the wrong path! ￣へ￣', status: 404 })
   }) 
 
+  fastify.get("/", async () => {
+    return { message: "Hi User <3 - AMS Backend Server", 
+      version: process.env.VERSION || "dev", 
+      timestamp: new Date().toISOString(),
+    };
+});
+
+  fastify.get("/health", async () => {
+    return { message: "OK.", 
+      version: process.env.VERSION || "dev", 
+      timestamp: new Date().toISOString(),
+      uptime: {
+        process: process.uptime().toFixed(0),
+        server: os.uptime().toFixed(0)
+      }
+    };
+});
   
 }
